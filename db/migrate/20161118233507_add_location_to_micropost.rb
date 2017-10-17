dbf1173c290790cb0de343755525b9682b9fae7b
Cooper Luetje <koopaluigi@hotmail.com>
2017-10-17 17:33:41 +0000

    Changed database migration

--- db/migrate/20161118233507_add_location_to_micropost.rb
+++ db/migrate/20161118233507_add_location_to_micropost.rb
@@ -1,5 +1,5 @@
 class AddLocationToMicropost < ActiveRecord::Migration[5.0]
   def change
-    add_column :microposts, :location, :location
+    add_column :microposts, :locations, :locations
   end
 end
